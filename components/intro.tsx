dd8f97ce67a27fe360044babe23aa94c1740a8e2
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2024-01-07 16:17:38 -0800

    Modified Components

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import  profilePic  from "@/public/profilePic.png";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -9,7 +8,7 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
-// import { useActiveSectionContext } from "@/context/active-section-context";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -32,7 +31,6 @@ export default function Intro() {
             }}
           >
             <Image
-              // src={profilePic}
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
               alt="Ricardo portrait"
               width="192"
